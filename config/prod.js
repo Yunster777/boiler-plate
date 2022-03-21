5d02608cb36603bfb63e7ed07f95f3baf42b5d48
Yunster777 <kysung14@Yunseongui-MacBookPro.local>
2022-03-21 13:28:59 +0900

    비밀설정

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+// Heroku 사이트에서 세팅한 값 사용
+module.exports = {
+    mongoURI: process.env.MONGO_URI
+}
--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+const config = require('./config/key')
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kysung14:Mrhdbstjd1$@bolier-plate.6mpl5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     // mongoose 6.0부터는 아래 설정을 기본 제공하기 때문에 사용하면 안됨
     //useNewRulParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World! 안녕하세요~'))
+app.get('/', (req, res) => res.send('Hello World! 안녕하세요~222'))
+
+// 회원가입
+app.post('/register', (req, res) => {
+    // 회원가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 저장
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
